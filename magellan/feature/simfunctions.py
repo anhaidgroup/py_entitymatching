88230fbf34bb96e12b9de6bc743c97715ad3c9ad
pradap <pradap@cs.wisc.edu>
2016-05-26 16:55:41 -0500

    working on validations for sim. and tok functions

--- magellan/feature/simfunctions.py
+++ magellan/feature/simfunctions.py
@@ -8,10 +8,9 @@ import magellan.externals.py_stringmatching.simfunctions as sim
 import magellan.externals.py_stringmatching.tokenizers as tok
 from magellan.utils.generic_helper import remove_non_ascii
 
-
-sim_fn_names=['jaccard', 'lev', 'cosine', 'monge_elkan',
-              'needleman_wunsch', 'smith_waterman', 'jaro', 'jaro_winkler',
-              'exact_match', 'rel_diff', 'abs_norm']
+sim_fn_names = ['jaccard', 'lev', 'cosine', 'monge_elkan',
+                'needleman_wunsch', 'smith_waterman', 'jaro', 'jaro_winkler',
+                'exact_match', 'rel_diff', 'abs_norm']
 
 # abbreviations for sim. functions
 abb = ['jac', 'lev', 'cos', 'mel',
@@ -19,30 +18,22 @@ abb = ['jac', 'lev', 'cos', 'mel',
        'exm', 'rdf', 'anm']
 
 # global function names
-_global_sim_fns = pd.DataFrame({'function_name':sim_fn_names, 'short_name':abb})
+_global_sim_fns = pd.DataFrame({'function_name': sim_fn_names, 'short_name': abb})
 
-# get similarity functions
-
-# def get_sim_funs_for_blocking():
-#     return get_sim_funs()
-#
-# def get_sim_funs_for_matching():
-#     return get_sim_funs()
-
-
-# get similarity functions
 
 def get_sim_funs_for_blocking():
     return get_sim_funs()
+
+
 def get_sim_funs_for_matching():
     return get_sim_funs()
 
+
 def get_sim_funs():
-    fns = [jaccard,  lev, cosine, monge_elkan, needleman_wunsch, smith_waterman,
+    fns = [jaccard, lev, cosine, monge_elkan, needleman_wunsch, smith_waterman,
            jaro, jaro_winkler,
            exact_match, rel_diff, abs_norm]
     return dict(zip(sim_fn_names, fns))
-
 
 
 def jaccard(arr1, arr2):
@@ -58,6 +49,7 @@ def jaccard(arr1, arr2):
         return np.NaN
     return sim.jaccard(set(arr1), set(arr2))
 
+
 def cosine(arr1, arr2):
     if arr1 is None or arr2 is None:
         return np.NaN
@@ -72,8 +64,8 @@ def cosine(arr1, arr2):
 
     return sim.cosine(arr1, arr2)
 
-def monge_elkan(arr1, arr2):
 
+def monge_elkan(arr1, arr2):
     if arr1 is None or arr2 is None:
         return np.NaN
     if not isinstance(arr1, list):
@@ -86,6 +78,7 @@ def monge_elkan(arr1, arr2):
         return np.NaN
 
     return sim.monge_elkan(arr1, arr2)
+
 
 # string based
 def lev(s1, s2):
@@ -111,6 +104,7 @@ def jaro(s1, s2):
         s2 = remove_non_ascii(s2)
     return sim.jaro(str(s1), str(s2))
 
+
 def jaro_winkler(s1, s2):
     if s1 is None or s2 is None:
         return np.NaN
@@ -121,7 +115,6 @@ def jaro_winkler(s1, s2):
     if isinstance(s2, basestring):
         s2 = remove_non_ascii(s2)
     return sim.jaro_winkler(str(s1), str(s2))
-
 
 
 def needleman_wunsch(s1, s2):
@@ -135,6 +128,7 @@ def needleman_wunsch(s1, s2):
         s2 = remove_non_ascii(s2)
     return sim.needleman_wunsch(s1, s2)
 
+
 def smith_waterman(s1, s2):
     if s1 is None or s2 is None:
         return np.NaN
@@ -145,7 +139,6 @@ def smith_waterman(s1, s2):
     if isinstance(s2, basestring):
         s2 = remove_non_ascii(s2)
     return sim.smith_waterman(s1, s2)
-
 
 
 # boolean/string/numeric similarity measure
@@ -159,6 +152,7 @@ def exact_match(d1, d2):
     else:
         return 0
 
+
 # numeric similarity measure
 def rel_diff(d1, d2):
     if d1 is None or d2 is None:
@@ -170,10 +164,11 @@ def rel_diff(d1, d2):
     if d1 == 0.0 and d2 == 0.0:
         return 0
     else:
-        x = abs(d1-d2)/(d1+d2)
+        x = abs(d1 - d2) / (d1 + d2)
         if x <= 10e-5:
             x = 0
         return 1.0 - x
+
 
 # compute absolute norm similarity
 def abs_norm(d1, d2):
@@ -186,9 +181,8 @@ def abs_norm(d1, d2):
     if d1 == 0.0 and d2 == 0.0:
         return 0
     else:
-        x =  1 - (abs(d1-d2)/max(d1, d2))
+        x = 1 - (abs(d1 - d2) / max(d1, d2))
 
         if x <= 10e-5:
             x = 0
         return x
-
--- magellan/feature/tokenizers.py
+++ magellan/feature/tokenizers.py
@@ -1,8 +1,13 @@
 # coding=utf-8
 import pandas as pd
+import logging
+import six
+
 
 import magellan.externals.py_stringmatching.tokenizers as tok
 from magellan.utils.generic_helper import remove_non_ascii
+
+logger = logging.getLogger(__name__)
 
 _global_tokenizers = pd.DataFrame({'function_name': ['tok_qgram', 'tok_delim', 'tok_wspce'],
                                    'short_name': ['qgm', 'dlm', 'wsp']})
@@ -11,29 +16,53 @@ _global_tokenizers = pd.DataFrame({'function_name': ['tok_qgram', 'tok_delim', '
 # Get a list of tokenizers that can be called with just input string as the argument
 
 def get_tokenizers_for_blocking(q=[2, 3], dlm_char=[' ']):
-    return get_single_arg_tokenizers(q, dlm_char)
+    if q is None and dlm_char is  None:
+        logger.error('Both q and dlm_char cannot be null')
+        raise AssertionError('Both q and dlm_char cannot be null')
+    else:
+        return get_single_arg_tokenizers(q, dlm_char)
+
 
 
 def get_tokenizers_for_matching(q=[2, 3], dlm_char=[' ']):
-    return get_single_arg_tokenizers(q, dlm_char)
+    if q is None and dlm_char is  None:
+        logger.error('Both q and dlm_char cannot be null')
+        raise AssertionError('Both q and dlm_char cannot be null')
+    else:
+        return get_single_arg_tokenizers(q, dlm_char)
 
 
 def get_single_arg_tokenizers(q=[2, 3], dlm_char=[' ']):
+    if q is None and dlm_char is  None:
+        logger.error('Both q and dlm_char cannot be null')
+        raise AssertionError('Both q and dlm_char cannot be null')
     # return function specific to given q and dlm_char
-    qgm_fn_list = [make_tok_qgram(k) for k in q]
-    dlm_fn_list = [make_tok_delim(k) for k in dlm_char]
-
-    qgm_names = ['qgm_' + str(x) for x in q]
-    dlm_names = ['dlm_dc' + str(i) for i in range(len(dlm_char))]
     names = []
-    names.extend(qgm_names)
-    names.extend(dlm_names)
-
     fns = []
-    fns.extend(qgm_fn_list)
-    fns.extend(dlm_fn_list)
+    if q is not None:
+        if len(q) > 0:
+            if not isinstance(q, list):
+                q = [q]
+            qgm_fn_list = [make_tok_qgram(k) for k in q]
+            qgm_names = ['qgm_' + str(x) for x in q]
+            names.extend(qgm_names)
+            fns.extend(qgm_fn_list)
 
-    return dict(zip(names, fns))
+    if dlm_char is not None:
+        if len(dlm_char) > 0:
+            if not isinstance(dlm_char, list) and isinstance(dlm_char, six.string_types):
+                dlm_char = [dlm_char]
+
+            dlm_fn_list = [make_tok_delim(k) for k in dlm_char]
+            dlm_names = ['dlm_dc' + str(i) for i in range(len(dlm_char))]
+            names.extend(dlm_names)
+            fns.extend(dlm_fn_list)
+
+    if len(names) > 0 and len(fns) > 0:
+        return dict(zip(names, fns))
+    else:
+        logger.warning('Didnot create any tokenizers, returning empty dict.')
+        return dict()
 
 
 # return a delimiter-based tokenizer with a fixed delimiter
