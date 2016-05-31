c23e6430210b142ac1f2e1b6d3f91c8574b0da42
Sanjib Das <sanjibkd@ernie.cs.wisc.edu>
2016-05-31 10:08:36 -0500

    [WIP] Updated benchmarks

--- benchmarks/benchmark_attr_equiv_blocker.py
+++ benchmarks/benchmark_attr_equiv_blocker.py
@@ -2,6 +2,7 @@
 # See "Writing benchmarks" in the asv docs for more information.
 
 import os                                                                       
+import sys
                                                                                 
 import magellan as mg                                                           
                                                                                 
@@ -10,19 +11,22 @@ class TimeBlockTables:
 	p = mg.get_install_path()                                                       
 	path_for_A = os.sep.join([p, 'datasets', 'test_datasets', 'blocker', 'bowker.csv'])                        
 	path_for_B = os.sep.join([p, 'datasets', 'test_datasets', 'blocker', 'walmart.csv'])                       
-	#l_key = 'ID'                                                                    
-	#r_key = 'ID'                                                                    
+	l_key = 'ID'                                                                    
+	r_key = 'ID'                                                                    
 	self.l_block_attr = 'pubYear'                                                       
 	self.r_block_attr = 'pubYear'                                                       
 	#l_output_attrs = ['pubYear']                                                   
 	#r_output_attrs = ['pubYear']                                                   
 	#l_output_prefix = 'ltable_'                                                     
 	#r_output_prefix = 'rtable_' 
-	self.A = mg.read_csv_metadata(path_for_A)                                        
-    	mg.set_key(A, l_key)                                                        
+	self.A = mg.read_csv_metadata(path_for_A)
+	#print sys.stderr, "Size of A: ", len(self.A)                                        
+    	mg.set_key(self.A, l_key)                                                        
     	self.B = mg.read_csv_metadata(path_for_B)                                        
-    	mg.set_key(B, r_key)                                                        
+	#print sys.stderr, "Size of B: ", len(self.B)                                        
+    	mg.set_key(self.B, r_key)                                                        
     	self.ab = mg.AttrEquivalenceBlocker()                                            
     
     def time_books(self):                                                 
-    	self.ab.block_tables(self.A, self.B, self.l_block_attr, self.r_block_attr)
+    	self.ab.block_tables(self.A, self.B, self.l_block_attr,
+			     self.r_block_attr, verbose=False)
