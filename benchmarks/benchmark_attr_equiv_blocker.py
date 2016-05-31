2979ec4e1281f6c7839edf692e3505e0da1f41a0
Sanjib Das <sanjibkd@ernie.cs.wisc.edu>
2016-05-31 12:39:35 -0500

    [WIP] Added benchmark test for block_candset

--- benchmarks/benchmark_attr_equiv_blocker.py
+++ benchmarks/benchmark_attr_equiv_blocker.py
@@ -6,7 +6,7 @@ import sys
                                                                                 
 import magellan as mg                                                           
                                                                                 
-class TimeBlockTables:                                                               
+class TimeBlockTablesBooks:                                                               
     def setup(self):
 	p = mg.get_install_path()                                                       
 	path_for_A = os.sep.join([p, 'datasets', 'test_datasets', 'blocker', 'bowker.csv'])                        
@@ -27,6 +27,45 @@ class TimeBlockTables:
     	mg.set_key(self.B, r_key)                                                        
     	self.ab = mg.AttrEquivalenceBlocker()                                            
     
-    def time_books(self):                                                 
+    def time_block_tables(self):                                                 
     	self.ab.block_tables(self.A, self.B, self.l_block_attr,
 			     self.r_block_attr, verbose=False)
+    def teardown(self):
+	del self.A
+	del self.B
+	del self.ab
+
+class TimeBlockCandsetBikes:
+    timeout = 3600.0                                                               
+    def setup(self):
+	p = mg.get_install_path()                                                       
+	path_for_A = os.sep.join([p, 'datasets', 'benchmark_datasets', 'bikes', 'A.csv'])                        
+	path_for_B = os.sep.join([p, 'datasets', 'benchmark_datasets', 'bikes', 'B.csv'])                        
+	l_key = 'ID'                                                                    
+	r_key = 'ID'                                                                    
+	self.A = mg.read_csv_metadata(path_for_A)
+    	mg.set_key(self.A, l_key)                                                        
+    	self.B = mg.read_csv_metadata(path_for_B)                                        
+    	mg.set_key(self.B, r_key)                                                        
+	l_block_attr_1 = 'city_posted'                                                       
+	r_block_attr_1 = 'city_posted'
+	l_output_attrs = ['bike_name', 'city_posted', 'km_driven', 'price',
+			  'color', 'model_year']                                                       
+	r_output_attrs = ['bike_name', 'city_posted', 'km_driven', 'price',
+			  'color', 'model_year']                                                       
+    	self.ab = mg.AttrEquivalenceBlocker()
+	self.C = self.ab.block_tables(self.A, self.B, l_block_attr_1,
+				      r_block_attr_1, l_output_attrs,
+				      r_output_attrs, verbose=False)                                            
+    	self.l_block_attr = 'model_year'
+	self.r_block_attr = 'model_year'
+
+    def time_block_candset(self):                                                 
+    	self.ab.block_candset(self.C, self.l_block_attr, self.r_block_attr,
+			      verbose=False, show_progress=False)
+
+    def teardown(self):
+	del self.A
+	del self.B
+	del self.C
+	del self.ab
