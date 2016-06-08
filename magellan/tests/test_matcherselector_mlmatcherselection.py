9590b5576d67cd9d7f11543e990094daeca3e949
kvpradap <pradap@cs.wisc.edu>
2016-06-07 21:57:31 -0500

    Updated mlmatcherselector testcases

--- magellan/tests/test_matcherselector_mlmatcherselection.py
+++ magellan/tests/test_matcherselector_mlmatcherselection.py
@@ -6,8 +6,6 @@ import six
 
 from magellan.utils.generic_helper import get_install_path, list_diff
 from magellan.io.parsers import read_csv_metadata
-from magellan.feature.autofeaturegen import get_features_for_matching
-from magellan.feature.extractfeatures import extract_feature_vecs
 from magellan.matcherselector.mlmatcherselection import select_matcher
 from magellan.matcher.dtmatcher import DTMatcher
 from magellan.matcher.linregmatcher import LinRegMatcher
@@ -18,10 +16,11 @@ from magellan.matcher.svmmatcher import SVMMatcher
 
 import magellan.catalog.catalog_manager as cm
 
-datasets_path = os.sep.join([get_install_path(), 'datasets', 'test_datasets'])
-path_a = os.sep.join([datasets_path, 'A.csv'])
-path_b = os.sep.join([datasets_path, 'B.csv'])
-path_c = os.sep.join([datasets_path, 'C.csv'])
+datasets_path = os.sep.join([get_install_path(), 'datasets', 'test_datasets', 'matcherselector'])
+path_a = os.sep.join([datasets_path, 'DBLP_demo.csv'])
+path_b = os.sep.join([datasets_path, 'ACM_demo.csv'])
+path_c = os.sep.join([datasets_path, 'dblp_acm_demo_labels.csv'])
+path_f = os.sep.join([datasets_path, 'feat_vecs.csv'])
 
 
 class MLMatcherSelectionTestCases(unittest.TestCase):
@@ -33,14 +32,12 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
     # @nottest
     def test_select_matcher_valid_1(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # C['labels'] = labels
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -50,8 +47,8 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         matchers = [dtmatcher, nbmatcher, rfmatcher, svmmatcher, linregmatcher, logregmatcher]
 
         result = select_matcher(matchers, x=None, y=None, table=feature_vectors,
-                                exclude_attrs=['ltable_ID', 'rtable_ID', '_id', 'labels'],
-                                target_attr='labels', k=2)
+                                exclude_attrs=['ltable.id', 'rtable.id', '_id', 'gold'],
+                                target_attr='gold', k=7)
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
         self.assertEqual(set(header) == set(list(result_df.columns[[0, 1, 2]])), True)
@@ -63,14 +60,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
     # @nottest
     def test_select_matcher_valid_2(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -81,9 +81,9 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         col_list = list(feature_vectors.columns)
         l = list_diff(col_list, [cm.get_key(feature_vectors), cm.get_fk_ltable(feature_vectors),
                                  cm.get_fk_rtable(feature_vectors),
-                                 'labels'])
+                                 'gold'])
         X = feature_vectors[l]
-        Y = feature_vectors['labels']
+        Y = feature_vectors['gold']
         result = select_matcher(matchers, x=X, y=Y)
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
@@ -96,14 +96,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
     # @nottest
     def test_select_matcher_valid_3(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -114,9 +117,9 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         col_list = list(feature_vectors.columns)
         l = list_diff(col_list, [cm.get_key(feature_vectors), cm.get_fk_ltable(feature_vectors),
                                  cm.get_fk_rtable(feature_vectors),
-                                 'labels'])
+                                 'gold'])
         X = feature_vectors[l]
-        Y = feature_vectors['labels']
+        Y = feature_vectors['gold']
         result = select_matcher(matchers, x=X, y=Y, metric='recall')
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
@@ -129,14 +132,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
     # @nottest
     def test_select_matcher_valid_4(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -147,9 +153,9 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         col_list = list(feature_vectors.columns)
         l = list_diff(col_list, [cm.get_key(feature_vectors), cm.get_fk_ltable(feature_vectors),
                                  cm.get_fk_rtable(feature_vectors),
-                                 'labels'])
+                                 'gold'])
         X = feature_vectors[l]
-        Y = feature_vectors['labels']
+        Y = feature_vectors['gold']
         result = select_matcher(matchers, x=X, y=Y, metric='f1')
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
@@ -162,14 +168,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
     # @nottest
     def test_select_matcher_valid_5(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -180,9 +189,9 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         col_list = list(feature_vectors.columns)
         l = list_diff(col_list, [cm.get_key(feature_vectors), cm.get_fk_ltable(feature_vectors),
                                  cm.get_fk_rtable(feature_vectors),
-                                 'labels'])
+                                 'gold'])
         X = feature_vectors[l]
-        Y = feature_vectors['labels']
+        Y = feature_vectors['gold']
         result = select_matcher(matchers, x=X, y=Y, metric='f1', k=4)
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
@@ -195,14 +204,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
 
     def test_select_matcher_valid_6(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -213,9 +225,9 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         col_list = list(feature_vectors.columns)
         l = list_diff(col_list, [cm.get_fk_ltable(feature_vectors),
                                  cm.get_fk_rtable(feature_vectors),
-                                 'labels'])
+                                 'gold'])
         X = feature_vectors[l]
-        Y = feature_vectors['labels']
+        Y = feature_vectors['gold']
         result = select_matcher(matchers, x=X, y=Y)
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
@@ -227,14 +239,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         self.assertEqual(p_max, a_max)
 
     def test_select_matcher_valid_7(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -251,7 +266,7 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
         result = select_matcher(matchers, x=None, y=None, table=feature_vectors,
                                 exclude_attrs='_id',
-                                target_attr='labels', k=2)
+                                target_attr='gold', k=2)
 
         header = ['Name', 'Matcher', 'Num folds']
         result_df = result['cv_stats']
@@ -273,14 +288,17 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
     @raises(AssertionError)
     def test_select_matcher_target_attr_not_series(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -291,21 +309,24 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
         col_list = list(feature_vectors.columns)
         l = list_diff(col_list, [cm.get_fk_ltable(feature_vectors),
                                  cm.get_fk_rtable(feature_vectors),
-                                 'labels'])
+                                 'gold'])
         X = feature_vectors[l]
-        Y = feature_vectors[['labels']]
+        Y = feature_vectors[['gold']]
         result = select_matcher(matchers, x=X, y=Y)
 
     @raises(AssertionError)
     def test_select_matcher_ex_attrs_not_present(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
@@ -322,18 +343,21 @@ class MLMatcherSelectionTestCases(unittest.TestCase):
 
         result = select_matcher(matchers, x=None, y=None, table=feature_vectors,
                                 exclude_attrs='_id1',
-                                target_attr='labels', k=2)
+                                target_attr='gold', k=2)
 
     @raises(AssertionError)
     def test_select_matcher_target_attr_not_present(self):
-        A = read_csv_metadata(path_a)
-        B = read_csv_metadata(path_b, key='ID')
-        C = read_csv_metadata(path_c, ltable=A, rtable=B)
-        labels = [0] * 7
-        labels.extend([1] * 8)
-        C['labels'] = labels
-        feature_table = get_features_for_matching(A, B)
-        feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='labels')
+        A = read_csv_metadata(path_a, key='id')
+        B = read_csv_metadata(path_b, key='id')
+        # C = read_csv_metadata(path_c, ltable=A, rtable=B, fk_ltable='ltable.id',
+        #                       fk_rtable='rtable.id', key='_id')
+        # labels = [0] * 7
+        # labels.extend([1] * 8)
+        # C['labels'] = labels
+        # feature_table = get_features_for_matching(A, B)
+        # feature_vectors = extract_feature_vecs(C, feature_table=feature_table, attrs_after='gold')
+        # feature_vectors.fillna(0, inplace=True)
+        feature_vectors = read_csv_metadata(path_f, ltable=A, rtable=B)
         dtmatcher = DTMatcher()
         nbmatcher = NBMatcher()
         rfmatcher = RFMatcher()
