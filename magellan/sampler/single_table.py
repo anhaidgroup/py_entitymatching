127fa51a4ee01ed3fb9309bdbc769f8c3534acb3
pradap <pradap@cs.wisc.edu>
2016-06-12 15:21:27 -0500

    Updated docstrings to labeler and sampler.

--- magellan/sampler/single_table.py
+++ magellan/sampler/single_table.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 """
-This module contains sampling related routines
+This module contains sampling related routines for a single table.
 """
-
 import logging
 
 import pandas as pd
@@ -15,21 +14,23 @@ logger = logging.getLogger(__name__)
 
 
 # sample one table using random sampling
-def sample_table(data_frame, sample_size, replace=False, verbose=False):
+def sample_table(table, sample_size, replace=False, verbose=False):
     """
     Sample a pandas DataFrame (for labeling purposes).
 
-    This function samples a DataFrame, to be typically used for labeling
+    This function samples a DataFrame, typically used for labeling
     purposes. This function expects the input DataFrame containing the
     metadata of a candidate set (such as key, fk_ltable, fk_rtable, ltable,
-    rtable). Currently this function  samples the data
-    using uniform random sampling. Specifically, this function uses 'random'
-    function from numpy to sample the DataFrame.
+    rtable). Specifically, this function creates a copy of the input
+    DataFrame, samples the data using uniform random sampling (uses 'random'
+    function from numpy to sample) and returns the sampled DataFrame.
+    Further, also copies the properties from the input DataFrame to the output
+    DataFrame.
 
     Args:
-        data_frame (DataFrame): Input DataFrame to be sampled. Specifically,
+        table (DataFrame): Input DataFrame to be sampled. Specifically,
             a DataFrame containing the metadata of a candidate set (such as
-            key, fk_ltable, fk_rtable, ltable, rtable).
+            key, fk_ltable, fk_rtable, ltable, rtable) in the catalog.
         sample_size (int): Number of samples to be picked up from the input
             DataFrame.
         replace (boolean): Flag to indicate whether sampling should be done
@@ -39,13 +40,14 @@ def sample_table(data_frame, sample_size, replace=False, verbose=False):
             False).
 
     Returns:
-        A DataFrame with 'sample_size' number of rows. Further, this function
-        sets the output DataFrame's properties same as input DataFrame.
+        A new DataFrame with 'sample_size' number of rows. Further,
+        this function sets the output DataFrame's properties same as input
+        DataFrame.
 
     Raises:
-        AssertionError: If the input object is not of type pandas DataFrame.
+        AssertionError: If the input table is not of type pandas DataFrame.
         AssertionError: If the input DataFrame size is 0.
-        AssertionError: If the sample_size id greater than the input
+        AssertionError: If the sample_size is greater than the input
             DataFrame size.
 
     Notes:
@@ -59,18 +61,18 @@ def sample_table(data_frame, sample_size, replace=False, verbose=False):
     # Validate input parameters.
 
     # # The input DataFrame is expected to be of type pandas DataFrame.
-    if not isinstance(data_frame, pd.DataFrame):
+    if not isinstance(table, pd.DataFrame):
         logger.error('Input table is not of type pandas dataframe')
         raise AssertionError('Input table is not of type pandas dataframe')
 
     # # There should at least not-zero rows to sample from
-    if len(data_frame) == 0:
+    if len(table) == 0:
         logger.error('Size of the input table is 0')
         raise AssertionError('Size of the input table is 0')
 
     # # The sample size should be less than or equal to the number of rows in
     #  the input DataFrame
-    if len(data_frame) < sample_size:
+    if len(table) < sample_size:
         logger.error('Sample size is larger than the input table size')
         raise AssertionError('Sample size is larger than the input table size')
 
@@ -84,19 +86,19 @@ def sample_table(data_frame, sample_size, replace=False, verbose=False):
 
     # # Second, get the metadata
     key, fk_ltable, fk_rtable, ltable, rtable, l_key, r_key = \
-        cm.get_metadata_for_candset(data_frame, logger, verbose)
+        cm.get_metadata_for_candset(table, logger, verbose)
 
     # # Third, validate the metadata
-    cm.validate_metadata_for_candset(data_frame, key, fk_ltable, fk_rtable,
+    cm.validate_metadata_for_candset(table, key, fk_ltable, fk_rtable,
                                      ltable, rtable, l_key, r_key,
                                      logger, verbose)
 
     # Get the sample set for the output table
-    sample_indices = pd.np.random.choice(len(data_frame), sample_size,
+    sample_indices = pd.np.random.choice(len(table), sample_size,
                                          replace=replace)
     # Sort the indices ordered by index value
     sample_indices = sorted(sample_indices)
-    sampled_table = data_frame.iloc[list(sample_indices)]
+    sampled_table = table.iloc[list(sample_indices)]
 
     # Copy the properties
     cm.init_properties(sampled_table)
@@ -104,7 +106,7 @@ def sample_table(data_frame, sample_size, replace=False, verbose=False):
     # # If the replace is set to True, then we should check for the validity
     # of key before setting it
     if replace:
-        properties = cm.get_all_properties(data_frame)
+        properties = cm.get_all_properties(table)
         for property_name, property_value in six.iteritems(properties):
             if property_name == 'key':
                 # Check for the validity of key before setting it
@@ -113,7 +115,7 @@ def sample_table(data_frame, sample_size, replace=False, verbose=False):
                 # Copy the other properties as is
                 cm.set_property(sampled_table, property_name, property_value)
     else:
-        cm.copy_properties(data_frame, sampled_table)
+        cm.copy_properties(table, sampled_table)
 
     # Return the sampled table
     return sampled_table
