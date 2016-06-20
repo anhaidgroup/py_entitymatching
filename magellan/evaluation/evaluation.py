43aef0f11bf69634c41c86e8215b0b14001a177b
pradap <pradap@cs.wisc.edu>
2016-06-20 15:36:37 -0500

    Updated docstrings for evaluation

--- magellan/evaluation/evaluation.py
+++ magellan/evaluation/evaluation.py
@@ -1,110 +1,187 @@
+"""
+This module contains evaluation related functions.
+"""
+import collections
 import logging
-from collections import OrderedDict
 
 import pandas as pd
 import six
 
 import magellan.catalog.catalog_manager as cm
-from magellan.utils.catalog_helper import check_attrs_present
+import magellan.utils.catalog_helper as ch
+import magellan.utils.generic_helper as gh
 
 logger = logging.getLogger(__name__)
 
 
-def eval_matches(X, gold_label_attr, predicted_label_attr):
-    if not isinstance(X, pd.DataFrame):
-        logger.error('The input table is not of type dataframe')
-        raise AssertionError('The input is not of type dataframe')
+def eval_matches(data_frame, gold_label_attr, predicted_label_attr):
+    """
+    Evaluates the matches from the matcher.
 
+    Specifically, given a DataFrame containing golden labels and predicted
+    labels, this function would evaluate the matches and return the accuracy
+    results such as precision, recall and F1.
+
+    Args:
+        data_frame (DataFrame): Input pandas DataFrame containing "gold"
+            labels and "predicted" labels.
+        gold_label_attr (str): An attribute in the input DataFrame containing
+            "gold" labels.
+        predicted_label_attr (str): An attribute in the input DataFrame
+            containing "predicted" labels.
+
+    Returns:
+        A python dictionary containing the accuracy measures such as
+        precision, recall, F1.
+
+    Raises:
+        AssertionError: If the input object (data_frame) is not of type
+            pandas DataFrame.
+        AssertionError: If the input attribute (gold_label_attr) is not of
+            type string.
+        AssertionError: If the input attribute (predicted_label_attr) is not of
+            type string.
+        AssertionError: If the input attribute (gold_label_attr) is not in
+            the input dataFrame.
+        AssertionError: If the input attribute (predicted_label_attr) is not in
+            the input dataFrame.
+    """
+    # Validate input parameters
+
+    # # We expect the input object to be of type pandas DataFrame
+    if not isinstance(data_frame, pd.DataFrame):
+        logger.error('The input table is not of type DataFrame')
+        raise AssertionError('The input is not of type DataFrame')
+
+    # # We expect the input attribute (gold_label_attr) to be of type string
     if not isinstance(gold_label_attr, six.string_types):
         logger.error('The input gold_label_attr is not of type string')
         raise AssertionError('The input gold_label_attr is not of type string')
 
+    # # We expect the input attribute (predicted_label_attr) to be of type
+    # string
     if not isinstance(predicted_label_attr, six.string_types):
         logger.error('The input predicted_label_attr is not of type string')
-        raise AssertionError('The input predicted_label_attr is not of type string')
+        raise AssertionError(
+            'The input predicted_label_attr is not of type string')
 
-    if not check_attrs_present(X, gold_label_attr):
-        logger.error('The gold_label_attr is not present in the input table')
-        raise AssertionError('The gold_label_attr is not present in the input table')
+    # Check whether the gold label attribute is present in the input table
+    if not ch.check_attrs_present(data_frame, gold_label_attr):
+        logger.error(
+            'The gold_label_attr is not present in the input DataFrame')
+        raise AssertionError(
+            'The gold_label_attr is not present in the input DataFrame')
 
-    if not check_attrs_present(X, predicted_label_attr):
-        logger.error('The predicted_label_attr is not present in the input table')
-        raise AssertionError('The predicted_label_attr is not present in the input table')
+    # Check whether the predicted label attribute is present in the input table
+    if not ch.check_attrs_present(data_frame, predicted_label_attr):
+        logger.error(
+            'The predicted_label_attr is not present in the input DataFrame')
+        raise AssertionError(
+            'The predicted_label_attr is not present in the input DataFrame')
 
-    Y = X.reset_index(drop=False, inplace=False)
-    g = Y[gold_label_attr]
-    # if isinstance(g, pd.DataFrame):
-    #     g = g.T
-    #     assert len(g) == 1, 'Error: Column is picked as dataframe and the num rows > 1'
-    #     g = g.iloc[0]
+    # Reset the index to get the indices set as 0..len(table)
+    new_data_frame = data_frame.reset_index(drop=False, inplace=False)
 
-    p = Y[predicted_label_attr]
-    # if isinstance(p, pd.DataFrame):
-    #     p = p.T
-    #     assert len(p) == 1, 'Error: Column is picked as dataframe and the num rows > 1'
-    #     p = p.iloc[0]
+    # Project out the gold and label attributes.
+    gold = new_data_frame[gold_label_attr]
+    predicted = new_data_frame[predicted_label_attr]
 
-    # get false label (0) indices
-    gf = g[g == 0].index.values
+    # Get gold negatives, positives
+    gold_negative = gold[gold == 0].index.values
+    gold_positive = gold[gold == 1].index.values
 
-    pf = p[p == 0].index.values
-
-    # get true label (1) indices
-    gt = g[g == 1].index.values
-
-    pt = p[p == 1].index.values
+    # Get predicted negatives, positives
+    predicted_negative = predicted[predicted == 0].index.values
+    predicted_positive = predicted[predicted == 1].index.values
 
     # get false positive indices
-    fp_indices = list(set(gf).intersection(pt))
+    false_positive_indices = \
+        list(set(gold_negative).intersection(predicted_positive))
 
     # get true positive indices
-    tp_indices = list(set(gt).intersection(pt))
+    true_positive_indices = \
+        list(set(gold_positive).intersection(predicted_positive))
 
     # get false negative indices
-    fn_indices = list(set(gt).intersection(pf))
+    false_negative_indices = \
+        list(set(gold_positive).intersection(predicted_negative))
 
     # get true negative indices
-    tn_indices = list(set(gf).intersection(pf))
+    true_negative_indices = \
+        list(set(gold_negative).intersection(predicted_negative))
 
-    n_tp = float(len(tp_indices))
-    n_fp = float(len(fp_indices))
-    n_fn = float(len(fn_indices))
-    n_tn = float(len(tn_indices))
-    prec_num = n_tp
-    prec_den = n_tp + n_fp
-    rec_num = n_tp
-    rec_den = n_tp + n_fn
-    if prec_den == 0.0:
+    # Get the number of TP, FP, FN, TN
+    num_true_positives = float(len(true_positive_indices))
+    num_false_positives = float(len(false_positive_indices))
+    num_false_negatives = float(len(false_negative_indices))
+    num_true_negatives = float(len(true_negative_indices))
+
+    # Precision = num_tp/ (num_tp + num_fp)
+
+    # Get precision numerator, denominator
+    precision_numerator = num_true_positives
+    precision_denominiator = num_true_positives + num_false_positives
+
+    # Precision = num_tp/ (num_tp + num_fn)
+    # Get recall numerator, denominator
+    recall_numerator = num_true_positives
+    recall_denominator = num_true_positives + num_false_negatives
+
+    # Compute precision
+    if precision_denominiator == 0.0:
         precision = 0.0
     else:
-        precision = prec_num / prec_den
-    if rec_den == 0.0:
+        precision = precision_numerator / precision_denominiator
+
+    # Compute recall
+    if recall_denominator == 0.0:
         recall = 0.0
     else:
-        recall = rec_num / rec_den
+        recall = recall_numerator / recall_denominator
 
+    # Compute F1
     if precision == 0.0 and recall == 0.0:
-        f1 = 0.0
+        F1 = 0.0
     else:
-        f1 = (2.0 * precision * recall) / (precision + recall)
+        F1 = (2.0 * precision * recall) / (precision + recall)
 
-    fk_ltable, fk_rtable = cm.get_property(X, 'fk_ltable'), cm.get_property(X, 'fk_rtable')
+    # Get the fk_ltable and fk_rtable
+    fk_ltable = cm.get_property(data_frame, 'fk_ltable')
+    fk_rtable = cm.get_property(data_frame, 'fk_rtable')
 
-    Y.set_index([fk_ltable, fk_rtable], drop=False, inplace=True)
-    false_pos_ls = list(Y.ix[fp_indices].index.values)
-    false_neg_ls = list(Y.ix[fn_indices].index.values)
-    ret_dict = OrderedDict()
-    ret_dict['prec_numerator'] = prec_num
-    ret_dict['prec_denominator'] = prec_den
-    ret_dict['precision'] = precision
-    ret_dict['recall_numerator'] = rec_num
-    ret_dict['recall_denominator'] = rec_den
-    ret_dict['recall'] = recall
-    ret_dict['f1'] = f1
-    ret_dict['pred_pos_num'] = n_tp + n_fp
-    ret_dict['false_pos_num'] = n_fp
-    ret_dict['false_pos_ls'] = false_pos_ls
-    ret_dict['pred_neg_num'] = n_fn + n_tn
-    ret_dict['false_neg_num'] = n_fn
-    ret_dict['false_neg_ls'] = false_neg_ls
-    return ret_dict
+    # Check if the fk_ltable contain any missing values
+    if not ch.does_contain_missing_vals(data_frame, fk_ltable):
+        logger.error('The fk_ltable (%s) contains missing values' %fk_ltable)
+        raise AssertionError('The fk_ltable (%s) contains missing values' %
+                             fk_ltable)
+
+    # Check if the fk_rtable contain any missing values
+    if not ch.does_contain_missing_vals(data_frame, fk_rtable):
+        logger.error('The fk_rtable (%s) contains missing values' %fk_rtable)
+        raise AssertionError('The fk_rtable (%s) contains missing values' %
+                             fk_rtable)
+
+
+    # Set the index values to fk_ltable and fk_rtable
+    new_data_frame.set_index([fk_ltable, fk_rtable], drop=False, inplace=True)
+
+    # Get the list of false positives and false negatives.
+    false_pos_ls = list(new_data_frame.ix[false_positive_indices].index.values)
+    false_neg_ls = list(new_data_frame.ix[false_negative_indices].index.values)
+
+    # Store and return the accuracy results.
+    accuracy_results = collections.OrderedDict()
+    accuracy_results['prec_numerator'] = precision_numerator
+    accuracy_results['prec_denominator'] = precision_denominiator
+    accuracy_results['precision'] = precision
+    accuracy_results['recall_numerator'] = recall_numerator
+    accuracy_results['recall_denominator'] = recall_denominator
+    accuracy_results['recall'] = recall
+    accuracy_results['f1'] = F1
+    accuracy_results['pred_pos_num'] = num_true_positives + num_false_positives
+    accuracy_results['false_pos_num'] = num_false_positives
+    accuracy_results['false_pos_ls'] = false_pos_ls
+    accuracy_results['pred_neg_num'] = num_false_negatives + num_true_negatives
+    accuracy_results['false_neg_num'] = num_false_negatives
+    accuracy_results['false_neg_ls'] = false_neg_ls
+    return accuracy_results
