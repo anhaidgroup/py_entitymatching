a3a5fcce76a9222927610ea787c6818cb0c182a5
kvpradap <pradap@cs.wisc.edu>
2016-07-13 23:01:04 -0500

    [BLD] Updated the setup.py and README.rst

--- README.md
+++
@@ -1,3 +0,0 @@
-# magellan_catalog
-
-Magellan with catalog for metadata
--- setup.py
+++ setup.py
@@ -1,45 +1,76 @@
-from setuptools import setup
-
-# Set this to True to enable building extensions using Cython.
-# Set it to False to build extensions from the C file (that
-# was previously created using Cython).
-# Set it to 'auto' to build with Cython if available, otherwise
-# from the C file.
 
 
-setup(
-    name='Magellan',
-    version='0.1',
-    description='End-to-End Entity Matching Management System',
-    long_description="""
-    EM is a critical problem and will become even more critical. Lot of EM research, but has focused mostly
-    on matching algorithms. Focus mostly on maximizing some performance factors such as accuracy, time, money, etc.
-    Currently there is very little help for users. For many of these steps, there is little or no work, so no
-    solution. Even when there are solutions, there may be no tools. Even when there are tools, user is faced
-    with a wide variety of tools, one for each step, so user is left trying to move among the tools, stitching them
-    together, decifering their different data formats, import/export commands. too cumbersome and difficult.
-    So often users just give up and roll their own ad-hoc solutions instead. This is obviously a major bottleneck
-    that prevents the deployment of matching in practice.
-    To solve the above problems, Magellan provides a set of commands that help the user to
-    1) Come up with a EM workflow
-    2) Iterate and debugmatcher the workflow
-    3) Deploy it in production
-    """,
-    url='http://github.com/anhaidgroup/magellan',
-    author='UW Magellan',
-    author_email='uwmagellan@gmail.com',
-    license=['BSD'],
-    packages=['magellan'],
-    install_requires=[
-                      'pandas >= 0.16.0',
-                      'six',
-                      'scikit-learn >= 0.16.1',
-                      'cloudpickle',
-                      'pyparsing >= 2.0.3',
-                      'PyPrind >= 2.9.3',
-                      'joblib',
-                      'py_stringmatching'
-                      ],
-    include_package_data=True,
-    zip_safe=False
-)
+# check if pip is installed. If not, raise an ImportError
+PIP_INSTALLED = True
+
+try:
+    import pip
+except ImportError:
+    PIP_INSTALLED = False
+
+if not PIP_INSTALLED:
+    raise ImportError('pip is not installed.')
+
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+# check if setuptools is installed. If not, install setuptools
+# automatically using pip.
+install_and_import('setuptools')
+
+if __name__ == "__main__":
+
+    # find packages to be included.
+    packages = setuptools.find_packages()
+
+    with open('README.rst') as f:
+        LONG_DESCRIPTION = f.read()
+
+    setuptools.setup(
+        name='py_stringsimjoin',
+        version='0.1.0',
+        description='Python library for end to end Entity Matching.',
+        long_description=LONG_DESCRIPTION,
+        url='https://sites.google.com/site/anhaidgroup/projects/magellan',
+        author='UW Magellan Team',
+        author_email='uwmagellan@gmail.com',
+        license='BSD',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Operating System :: MacOS',
+            'Operating System :: Microsoft :: Windows',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Utilities',
+            'Topic :: Software Development :: Libraries',
+        ],
+        packages=packages,
+        install_requires=[
+            'py_stringsimjoin', # this internally installs other
+            # dependencies such as py_stringmatching, joblib, pyprind
+            'cloudpickle',
+            'pyparsing',
+            'scikit-learn'
+        ],
+        include_package_data=True,
+        zip_safe=False
+    )
