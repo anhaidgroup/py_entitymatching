2f5ecc5266ea773bad5525701d8a9b465c08ddb5
kvpradap <pradap@cs.wisc.edu>
2016-07-15 09:25:57 -0500

    [CLN] Removed unnecessary files

--- garage/test_attr_types.py
+++
@@ -1,6 +0,0 @@
-import magellan as mg
-
-A = mg.load_dataset('table_A', key='ID')
-A1= A[['ID']]
-d = mg.get_attr_types(A1)
-print(d)
--- garage/test_delfiles.py
+++
@@ -1,7 +0,0 @@
-import os
-import magellan as mg
-
-from magellan.utils.generic_helper import del_files_in_dir
-p = os.sep.join([mg.get_install_path(), 'datasets','test_datasets', 'sandbox'])
-
-del_files_in_dir(p)
