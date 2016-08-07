417e878260c8a2a7220044263a3c0ac7fe50e714
pradap <pradap@cs.wisc.edu>
2016-08-07 14:07:31 -0500

    [IPY] Updated the demo notebook.

--- notebooks/how-to-guides/Demo/custom_blocker.py
+++ notebooks/how-to-guides/Demo/custom_blocker.py
@@ -2,18 +2,18 @@
 import py_entitymatching.catalog.catalog_manager as cm
 from py_entitymatching.blocker.attr_equiv_blocker import AttrEquivalenceBlocker
 
-class MyBlocker(AttrEquivalenceBlocker):
+class CustomBlocker(AttrEquivalenceBlocker):
     def __init__(self, *args, **kwargs):
         super(AttrEquivalenceBlocker, self).__init__(*args, **kwargs)
-        self.black_box_function = None
-    def set_blackbox_function(self, function):
-        self.black_box_function = function
+    def extract_first_two_digits(self, x):
+        return x//100
+        
     def block_tables(self, A, B, l_block_attr, r_block_attr):
         A1, B1 = A.copy(), B.copy()
-        A1['l_tmp_attr'] = A[l_block_attr].map(self.black_box_function)
-        B1['r_tmp_attr'] = B[r_block_attr].map(self.black_box_function)
+        A1['l_tmp_attr'] = A[l_block_attr].map(self.extract_first_two_digits)
+        B1['r_tmp_attr'] = B[r_block_attr].map(self.extract_first_two_digits)
         cm.copy_properties(A, A1)
         cm.copy_properties(B, B1)
-        return super(MyBlocker, self).block_tables(A1, B1,'l_tmp_attr', 'r_tmp_attr',
+        return super(CustomBlocker, self).block_tables(A1, B1,'l_tmp_attr', 'r_tmp_attr',
                                                    l_output_attrs=[l_block_attr],
                                                    r_output_attrs=[r_block_attr])
