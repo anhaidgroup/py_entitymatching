85d12107929f6afc748ae8e75ef60e8ef69b7212
Ran Tan <wonderofaboy@outlook.com>
2018-03-25 12:47:07 -0400

    add tests to scalers module

--- py_entitymatching/feature/scalers.py
+++ py_entitymatching/feature/scalers.py
@@ -3,7 +3,9 @@ This module contains functions for re-scaling/normalizing features.
 """
 import six
 import logging
-
+import numpy as np
+import pandas as pd
+from py_entitymatching.utils.validation_helper import validate_object_type
 from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler
 from py_entitymatching.feature.attributeutils import get_attrs_to_project
 
@@ -11,19 +13,30 @@ from py_entitymatching.feature.attributeutils import get_attrs_to_project
 logger = logging.getLogger(__name__)
 
 
-def scale_features(table=None, exclude_attrs=None,
+def scale_features(table, exclude_attrs=None,
                    scaling_method=None, scaler=None):
     """
     Scale features with the specified scaling function.
     """
+    # Validate the input parameters
+    # We expect the input object ltable to be of type pandas DataFrame
+    validate_object_type(table, pd.DataFrame, 'Input table')
+
+    # Get projected table
     project_attrs = get_attrs_to_project(table=table, exclude_attrs=exclude_attrs)
+    # Check any non-numeric columns in the table
+    for attr in project_attrs:
+        if not np.issubdtype(table[attr].dtype, np.number):
+            raise AssertionError('Projected columns must be numeric vectors')
+
+    # Scale vectors contained in the projected table and return both features and scaler
     table[project_attrs], scaler = scale_vectors(table[project_attrs],
                                                  scaling_method, scaler)
 
     return table, scaler
 
 
-def scale_vectors(x, scaling_method=None, scaler=None):
+def scale_vectors(x: object, scaling_method: object = None, scaler: object = None) -> object:
     """
     Scale X with the specified scaling function.
     """
