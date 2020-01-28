8937d172a7e4029f0bd0240ed8ee08c6ee9430c0
Matthew Christie <christiemj09@gmail.com>
2020-01-28 11:56:44 -0600

    Packages required at dependency build time (patch).

--- ci-scripts/install-src.sh
+++ ci-scripts/install-src.sh
@@ -10,6 +10,11 @@ source activate py_entitymatching_test_env
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then conda install --yes gcc; fi
 which gcc
 
+
+# Packages required at dependency build time (patch)
+pip install numpy==1.16.2
+pip install scikit-learn==0.20
+
 # Package dependencies
 pip install -r requirements.txt
 
