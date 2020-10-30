8a55a0cc2bc4ebec9346614811f1963028b268a4
Matt Christie <christiemj09@gmail.com>
2020-10-30 16:55:29 -0500

    Drop support for Python 2.7, 3.5; add support for 3.8.

--- ci-scripts/install-src.sh
+++ ci-scripts/install-src.sh
@@ -9,10 +9,6 @@ source activate py_entitymatching_test_env
 # System dependencies
 which gcc
 
-# Packages required at dependency build time (patch)
-pip install numpy==1.16.2
-pip install scikit-learn==0.20
-
 # Package dependencies
 pip install -r requirements.txt
 
