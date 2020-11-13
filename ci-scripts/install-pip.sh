9cb94bdb88db0be862bd4234fce758e48a49c64d
Matt Christie <christiemj09@gmail.com>
2020-11-13 10:55:15 -0600

    Remove old pinned dependencies.

--- ci-scripts/install-pip.sh
+++ ci-scripts/install-pip.sh
@@ -6,10 +6,6 @@ set -e
 
 source activate py_entitymatching_test_env
 
-# Packages required at dependency build time (patch)
-pip install numpy==1.16.2
-pip install scikit-learn==0.20
-
 # Package dependencies (TODO: factor this out)
 pip install -r requirements.txt
 
