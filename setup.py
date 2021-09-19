9390e6444e2dc0976405fad4678f96cd98680b92
Matt Christie <33204186+christiemj09@users.noreply.github.com>
2021-09-19 16:14:41 -0500

    Add CI for Python 3.9. (#151)
    * Add Python 3.9 to CI.
    
    * Add Python 3.9 support to docs.

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Topic :: Scientific/Engineering',
             'Topic :: Utilities',
             'Topic :: Software Development :: Libraries',
