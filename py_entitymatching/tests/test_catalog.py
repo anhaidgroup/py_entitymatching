44975de6968f8f24f472742a392218b0bac3606c
Anson-Doan <anson.ddoan@gmail.com>
2023-03-16 17:59:28 -0500

    Update test_catalog.py

--- py_entitymatching/tests/test_catalog.py
+++ py_entitymatching/tests/test_catalog.py
@@ -1,8 +1,9 @@
 import os
-from nose.tools import *
+# from nose.tools import *
 import unittest
 import pandas as pd
 import numpy as np
+from .utils import raises
 
 from py_entitymatching.utils.generic_helper import get_install_path
 import py_entitymatching.catalog.catalog_manager as cm
