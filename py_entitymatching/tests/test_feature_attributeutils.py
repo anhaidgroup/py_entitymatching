f3f932cf14914b0d6abe907d0374f19a08485ed5
Anson-Doan <anson.ddoan@gmail.com>
2023-03-16 18:01:08 -0500

    Update test_feature_attributeutils.py

--- py_entitymatching/tests/test_feature_attributeutils.py
+++ py_entitymatching/tests/test_feature_attributeutils.py
@@ -1,8 +1,9 @@
 import os
-from nose.tools import *
+# from nose.tools import *
 import unittest
 import pandas as pd
 import six
+from .utils import raises
 
 from py_entitymatching.utils.generic_helper import get_install_path
 from py_entitymatching.io.parsers import read_csv_metadata
