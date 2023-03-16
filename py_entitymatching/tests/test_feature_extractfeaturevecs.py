198140c65724d81336c0a92fe4dfaaddebc4eb08
Anson-Doan <anson.ddoan@gmail.com>
2023-03-16 18:01:44 -0500

    Update test_feature_extractfeaturevecs.py

--- py_entitymatching/tests/test_feature_extractfeaturevecs.py
+++ py_entitymatching/tests/test_feature_extractfeaturevecs.py
@@ -1,7 +1,8 @@
 import os
-from nose.tools import *
+# from nose.tools import *
 import unittest
 import pandas as pd
+from .utils import raises
 
 from py_entitymatching.utils.generic_helper import get_install_path
 from py_entitymatching.io.parsers import read_csv_metadata
