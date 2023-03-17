dc847d58e95b845a3caf3cae0db5ddc4561589e7
Anson-Doan <anson.ddoan@gmail.com>
2023-03-17 12:09:10 -0500

    Update debugblocker_cython.cpp

--- py_entitymatching/debugblocker/debugblocker_cython.cpp
+++ py_entitymatching/debugblocker/debugblocker_cython.cpp
@@ -159,7 +159,7 @@
 #define CYTHON_FAST_PYCCALL  (CYTHON_FAST_PYCALL && PY_VERSION_HEX >= 0x030600B1)
 #endif
 #if CYTHON_USE_PYLONG_INTERNALS
-  #include "longintrepr.h"
+//  #include "longintrepr.h"
   #undef SHIFT
   #undef BASE
   #undef MASK
